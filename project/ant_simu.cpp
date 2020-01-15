929659f53e900c541211b6df510ac04cfc896b4b
David <davidvo22@hotmail.com>
2020-01-15 19:46:25 +0100

    show food quantity quand il arrive a 10,000

--- project/ant_simu.cpp
+++ project/ant_simu.cpp
@@ -97,7 +97,7 @@ int main(int nargs, char* argv[])
 
             end2 = std::chrono::system_clock::now();
             std::chrono::duration<double> time2 = end2-start2;
-            std::cout << "temps pour 10000 de nourriture : " << time2.count()  << std::endl;
+            std::cout << "temps pour " << food_quantity << " de nourriture : " << time2.count()  << std::endl;
             Switch_end = false;
         }
     });
